a3e153a0241b063f4517af7785b104a8ee90f10b
minabahadori <mbahahdori@potomacschool.org>
2021-11-15 09:46:13 -0500

    bird/photos

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ import pygame
 from pygame.locals import *
 
 #bird = pygame.image.load(flappy bird.png).convert()
+player = 'gallery/images/flappy bird.png'
+background = 'gallery/images/background.jpeg'
+
+
+fps = 64
+screen_width = 289
+screen_height = 511
+screen = pygame.display.set_mode((screen_width, screen_height))
+ground_y = screen_height*0.8
+game_images = {}
+game_sounds = {}
